80fb7d47cee6b68f9cf0ffd0d0c3985efacce0ae
Bubbl33s <jairo.lf.8@gmail.com>
2024-08-29 19:53:10 -0500

    mini proyecto finalizado

--- Semana 9/mini proyecto - plantilla/assets/js/script.js
+++ Semana 9/mini proyecto - plantilla/assets/js/script.js
@@ -1,8 +1,9 @@
 // Mini proyecto
 // Inicializamos nuestro arreglo con nombres de marcas de vehículos
-const cars = ["bmw", "volvo", "saab", "ford", "fiat", "audi"];
+let cars = ["bmw", "volvo", "saab", "ford", "fiat", "audi"];
 
 // Métodos de array
+// Push
 let btnPush = document.getElementById("btn-push");
 // Asignamos la función al botón
 btnPush.onclick = funcionPush;
@@ -14,7 +15,42 @@ function funcionPush() {
   mostrarImagenes();
 }
 
-// Esta funcion carga las imagenes en el elemento demo
+// Push
+let btnPop = document.getElementById("btn-pop");
+// Asignamos la función al botón
+btnPop.onclick = funcionPop;
+
+function funcionPop() {
+  if (cars.length > 0) {
+    // Actualizamos el array
+    cars.pop();
+    // Cargamos las imágenes nuevamente
+    mostrarImagenes();
+  } else {
+    alert("No se puede eliminar más elementos ya que el arreglo está vacío");
+  }
+}
+
+// slice
+let btnSlice = document.getElementById("btn-slice");
+btnSlice.onclick = funcionSlice;
+
+function funcionSlice() {
+  let begin = document.getElementById("begin").value;
+  let end = document.getElementById("end").value;
+
+  // Nos preguntamos si no son strings vacías
+  if (begin != "" && end != "") {
+    // Actualizamos el array
+    cars = cars.slice(begin, end);
+    // Cargamos las imágenes nuevamente
+    mostrarImagenes();
+  } else {
+    alert("Los campos no pueden estar vacíos");
+  }
+}
+
+// Esta funcion carga las imágenes en el elemento demo
 function mostrarImagenes() {
   let text = "";
 
